50ff1c43de79cbacdaf673e54a468bcca9af8ea6
DevAntonioAzevedo <antonio_azevedo@hotmail.com.br>
2025-08-01 15:56:50 -0300

    Projeto concluido, colocando projeto no Github

---
+++ app/src/main/java/com/example/motivacional/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.motivacional
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.motivacional.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val lista = resources.getStringArray(R.array.frases_motivacionais)
+
+        binding.btnGenerator.setOnClickListener {
+            val indice = Random.nextInt(lista.size)
+            binding.txtResult.text = lista[indice]
+        }
+    }
+}
